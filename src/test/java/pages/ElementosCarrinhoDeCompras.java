44b90e2d2e38c36c482fed79b01b7cc22fd4c279
Thiago Alan <thiagoalan9080@gmail.com>
2018-04-10 22:23:43 -0300

    alteração do arquivo ElementoCarrinhDeCompras

--- src/test/java/pages/ElementosCarrinhoDeCompras.java
+++ src/test/java/pages/ElementosCarrinhoDeCompras.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -22,44 +20,11 @@ public class ElementosCarrinhoDeCompras extends BaseUrl {
         return this;
     }
 
-    public ElementosCarrinhoDeCompras qtdProdutos(String qtdProdMassa){
+    public ElementosCarrinhoDeCompras qtdProdutos(String qtdProdMassa) {
 
-        WebElement qtdProdutos = driverNagator.findElement(By.xpath("//div[@class='form-control select__quantity']"));
-        String valorProduto = driverNagator.findElement(By.xpath("//span[@class='basket-productPrice']")).getText();
-
-        List<String> textoValor = Arrays.asList(Pattern.compile("R$").split(valorProduto));
-/*
-        String input = "st1:st2:st3";
-        System.out.println(Arrays .asList(Pattern.compile(":").split(input)));
-        System.out.println(Arrays.asList(Pattern.compile(":").split(input, 2)));
-
-        Aqui, o segundo sysout é um exemplo do método de separação com o argumento limite.
-        A saída do trecho acima é [st1, st2, st3][st1, st2:st3]
-*/
-        String valorUnitarioProduto = valorProduto.s
-        new Select(qtdProdutos).selectByVisibleText(qtdProdMassa);
-
-
-
-        switch (qtdProdMassa){
-            case 1 :
-                1 * 1;
-                break;
-            case 2:
-                int qtdProdMassa * 1;
-                break;
-            case 3:
-                int qtdProdMassa * 1;
-                break;
-            case 4:
-                default:
-
-        }
-
-        return this;
     }
 
-    public ElementosCarrinhoDeCompras calculaFrete(String cep , String valorFrete){
+    public ElementosCarrinhoDeCompras calculaFrete(String cep, String valorFrete){
 
         return this;
     }
